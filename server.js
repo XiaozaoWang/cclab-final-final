b0f44e8d03b999c4d09caadfc16bb2e6fe3dc237
Glitch (pacey-first-node) <none>
2023-04-29 14:07:27 +0000

    🎳👁 Checkpoint ./package.json:15785832/3681 ./server.js:15785832/1243

--- server.js
+++ server.js
@@ -4,8 +4,9 @@
 const express = require("express");
 const app = express(); // show a website to the clients
 
-app.use( express.static("public") ); // use things inside the public folder
-app.get("/", function (request, response) {      //req a html page
+app.use(express.static("public")); // use things inside the public folder
+app.get("/", function (request, response) {
+  //req a html page
   response.sendFile(__dirname + "views/index.html");
 });
 
@@ -14,15 +15,9 @@ const http = require("http");
 //const hostname = "127.0.0.1"; // localhost
 const port = 3000;
 const server = http.createServer(app);
-//server.listen(port, hostname, function() {}); 
-server.listen(port, function() {
+//server.listen(port, hostname, function() {});
+server.listen(port, function () {
   console.log("Server is running: Port: " + port);
-}); 
+});
 
-
-
-
-
-
-
-
+console.log("test");
