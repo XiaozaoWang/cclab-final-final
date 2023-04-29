2ed54d12f3eda360551751b6573e7daa4c3eb8ba
Glitch (glitch-hello-node) <none>
2023-04-29 13:56:25 +0000

    💅🍘 Checkpoint ./server.js:15785832/368:57595472/127

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 // node.js
 
-asdlfjas;ldkfj
+// express app
+const express = require("express");
+const app = express(); // show a website to the clients
 
-sjsjs
+app.use( express.static("public") ); // use things inside the public folder
+app.get("/", function (request, response) {      //req a html page
+  response.sendFile(__dirname + "views/index.html");
+});
+
+// HTTP Server
+const http = require("http");
+//c
+
+
+
+
+
+
